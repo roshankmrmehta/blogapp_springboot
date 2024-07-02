c8408ffa4f370088bf950097443071f2f737029b
Roshan Kumar Mehta <40052937+roshankmrmehta@users.noreply.github.com>
2024-07-02 23:07:45 +0530

    Spring Data JPA unit testing for entities and repositories

---
+++ src/test/java/com/scaler/blogapp/users/UsersRepositoryTests.java
@@ -0,0 +1,43 @@
+package com.scaler.blogapp.users;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class UsersRepositoryTests {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    @Order(1)
+    void can_create_users(){
+
+        var user = UserEntity.builder()
+                .username("Roshan")
+                .email("roshan@gmail.com")
+                .build();
+
+        usersRepository.save(user);
+    }
+
+    @Test
+    @Order(2)
+    void can_find_users(){
+        var user = UserEntity.builder()
+                .username("Roshan")
+                .email("roshan@gmail.com")
+                .build();
+
+        usersRepository.save(user);
+
+        var users = usersRepository.findAll();
+        assert  users.size()==1;
+
+        // Assertions.assertEquals(1, users.size());
+    }
+}
